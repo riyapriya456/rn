d5a983fa38327e9c49ac629a4bd9310f548b145b
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-16 13:32:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repository"
-git clone https://github.com/KR-BOTZ/KR-RENAMER-BOT1 /KR-RENAMER-BOT1
-cd /KR-RENAMER-BOT1 
+git clone https://github.com/KR-BOTZ/kr-renamer-bot /kr-renamer-bot
+cd /kr-renamer-bot 
 echo "installing requirements"
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
