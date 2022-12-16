514e4faa1a955ac8490e2f70eb7a438fc0066831
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-16 11:11:21 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -1,5 +1,6 @@
 import math
 import time
+from helper.txt import kr
 
 
 async def progress_for_pyrogram(
@@ -24,11 +25,11 @@ async def progress_for_pyrogram(
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "[{0}{1}] \n**Progress**: {2}%\n".format(
-            ''.join(["●" for i in range(math.floor(percentage / 5))]),
-            ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["⦿" for i in range(math.floor(percentage / 5))]),
+            ''.join(["⭗" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
+        tmp = progress + kr.PROGRESS_BAR.format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
