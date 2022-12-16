f42ce13da8297e5ca13e06af8ed8d21ece8d62b9
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-16 12:33:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "ea9db4503ed7088b788e06dfd818e00e")
 STRING = os.environ.get("STRING", "BQDMOvAAtCL0AccvASgo8J9mPqZUsZn2_kdxMSoHDOMD0j4m1PyNPdMdHvg_frq1psssdtKbEnjNyK41nqV-QmyScKmSOmIomNuS87QkcqBOk4cZYhn5hSZxREAd6kBrc3Mmrqo7IvnVbkQBmHvyIWVmIhUV_HsVw81mM6vLCBYOhoaVw7AR4pdsX7n8ugaYsavw-H5NMwfCVjsi4TITG5tFsx2CngQn-f5CGEigeEIVhVBPB8u9eV9Sdc5w3IGhV9J6oD2ZQXeXou8rI1y9-p5LEQS3uthcgXernvRK0jm8liTTW-sm24djVh9GeJaUnlauqwldnI25opf6eWGNqk7-ThLV4AAAAABZsWSfAA")
 
 
-bot = Client(
+botssss = Client(
 
            "Renamer",
 
@@ -27,7 +27,7 @@ bot = Client(
            
 
 if STRING:
-    apps = [Client2,bot]
+    apps = [Client2,botssss]
     for app in apps:
         app.start()
     idle()
@@ -35,4 +35,4 @@ if STRING:
         app.stop()
     
 else:
-    bot.run()
+    botssss.run()
