9036054fc96ac9d2a1639b06f99922bf8b424d10
Logesh <logesh.12a3@gmail.com>
2022-12-16 15:03:19 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','5377834462:AAHMIicrsgAhYS4_bKnp2tLHwg9U4eilRnE')
+token = os.environ.get('TOKEN','5860785519:AAGLJ9ionJJvRWq2EV8lfo6TlSvZ9JDgaUY')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
@@ -10,4 +10,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Tᴏᴛᴀʟ Usᴇʀ:- {total_user()}\nBᴏᴛ :- @KR_Renamer_Bot \n ᴄʀᴇᴀᴛᴇʀ :- @mrlokaman\nʟᴀɴɢᴜᴀɢᴇ :-ᴘʏᴛʜᴏɴ𝟹\n ʟɪʙʀᴀʀʏ :- ᴘʏʀᴏɢʀᴀᴍ 2.0\n sᴇʀᴠᴇʀ : - 𝙑𝙋𝙎 \n ᴛᴏᴛᴀʟ ʀᴇɴᴀᴍᴇᴅ ғɪʟᴇ :-{total_rename}\n ᴛᴏᴛᴀʟ sɪᴢᴇ ʀᴇɴᴀᴍᴇᴅ :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"Tᴏᴛᴀʟ Usᴇʀ:- {total_user()}\nBᴏᴛ :- @ls_Renamer_Bot \n ᴄʀᴇᴀᴛᴇʀ :- @LOGESH_BOTS\nʟᴀɴɢᴜᴀɢᴇ :-ᴘʏᴛʜᴏɴ𝟹\n ʟɪʙʀᴀʀʏ :- ᴘʏʀᴏɢʀᴀᴍ 2.0\n sᴇʀᴠᴇʀ : - 𝙑𝙋𝙎 \n ᴛᴏᴛᴀʟ ʀᴇɴᴀᴍᴇᴅ ғɪʟᴇ :-{total_rename}\n ᴛᴏᴛᴀʟ sɪᴢᴇ ʀᴇɴᴀᴍᴇᴅ :- {humanbytes(int(total_size))} ",quote=True)
