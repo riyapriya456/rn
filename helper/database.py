2303f16c876f4c9ff24b3414ff64734607846780
Logesh <logesh.12a3@gmail.com>
2022-12-16 15:17:57 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -2,7 +2,7 @@ import pymongo
 import os
 from helper.date import add_date
 DB_NAME = os.environ.get("DB_NAME","Cluster0")
-DB_URL = os.environ.get("DB_URL","mongodb+srv://Logesh:logesh2004@cluster0.fs295gw.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Logesh:Logesh2004@cluster0.fs295gw.mongodb.net/?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
