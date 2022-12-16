49bd113b7e9cc948a5f768dcd49472d7a36d1c15
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-16 13:55:58 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -12,19 +12,50 @@ class kr(object):
 ┃
 ╰─━━─❪ <b>@KR_Botz</b> ❫─━━─⍟ """
 
-    ABOUT_TXT = """
-<b>╭────[ 𝗔𝗕𝗢𝗨𝗧 𝗠𝗘 ]────⍟
-├🤖 𝐌ʏ 𝐍ᴀᴍᴇ:</b> {}
-<b>├👑 𝐎ᴡɴᴇʀ : <a href=https://t.me/MrTamil_KiD>𝙈𝙍.𝙏𝘼𝙈𝙄𝙇_𝙆𝙄𝘿</a>
-├😎 𝐃ᴇᴠs : <a href=https://t.me/MrTamil_KiD>❦︎𝐇ᴀᴘᴘʏ❦︎</a>
-├📕 𝐋ɪʙʀᴀʀʏ : 𝐘ʀᴏɢʀᴀᴍ
-├✏️ 𝐋ᴀɴɢᴜᴀɢᴇ : 𝐏ʏᴛʜᴏɴ 𝟹
-├💾 𝐃ᴀᴛᴀ 𝐁ᴀsᴇ : 𝐌ᴏɴɢᴏ ᴅʙ
-├🌀 𝐌ʏ 𝐒ᴇʀᴠᴇʀ : 𝐇ᴇʀᴏᴋᴜ
-├💡 Cʀᴇᴅɪᴛs : -------
-├📊 𝐁ᴜɪʟᴅ 𝐒ᴛᴀᴜs : 𝟷.𝟸.𝟶 [ 𝐁ᴇᴛᴀ ]              
-╰───────────────⍟ </b>
-                                """
+    ABOUT_TXT = """<b>╔══❰ 𝗥𝗘𝗡𝗔𝗠𝗘𝗥 𝗕𝗢𝗧 ❱═❍
+║╭━━━━━━━━━━━━━━━➣
+║┣⪼🤖 Mʏ Nᴀᴍᴇ : {}
+║┣⪼👦 Oᴡɴᴇʀ : <a href=https://t.me/MR_tamil_kid>Ꮋ ค ℘ ℘ ꪗ 👻 Ҝiᗪ</a>
+║┣⪼👨‍💻 Dᴇᴠ : <a href=https://t.me/LastDrogz>Lᴀsᴛ 🐲 Dʀᴏɢᴢ</a>
+║┣⪼📢 Uᴘᴅᴀᴛᴇ : <a href=https://t.me/kr_botz>𝗞𝗥 ⚠︎ 𝗕ᴏᴛᴢ</a>
+║┣⪼❣️ Sᴜᴘᴘᴏʀᴛ : <a href=https://t.me/kr_join>𝗞𝗥 👽 𝗝ᴏɪɴ</a>
+║┣⪼📡 Sᴇʀᴠᴇʀ : <a href=https://t.me/MRtamil_kid>𝗩𝗣𝗦</a>
+║┣⪼🗣️ Lᴀɴɢᴜᴀɢᴇ : <a href=https://www.python.org>Pʏᴛʜᴏɴ3</a>
+║┣⪼📚 Lɪʙʀᴀʀʏ : <a href=https://github.com/pyrogram>Pʏʀᴏɢʀᴀᴍ</a>  
+║┣⪼🗒️ Vᴇʀsɪᴏɴ : V 1.0.0 [ Bᴇᴛᴀ ]
+║╰━━━━━━━━━━━━━━━➣
+╚═════❰ @KR_Botz ❱═════❍ </b>
+"""
+
+
+    ABOUT__TXT = """<b>╭─━━━─❪ 𝗔𝗕𝗢𝗨𝗧 𝗠𝗘 ❫─━━━─⍟
+┃
+┣⪼ Mʏ ɴᴀᴍᴇ : {}
+┃
+┣⪼ Oᴡɴᴇʀ : <a href='https://telegram.dog/happy_kid_robot'>Hᴀᴘᴘʏ 🧛‍♂️ Kɪᴅ</a>
+┃
+┣⪼ Dᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://telegram.dog/LastDrogz'>Lᴀsᴛ 🐲 Dʀᴏɢᴢ</a>
+┃
+┣⪼ Cʜᴀɴɴᴇʟ :<a href='https://t.me/kr_botz'>KR ⚠︎ Bᴏᴛᴢ</a>
+┃
+┣⪼ Sᴜᴘᴘᴏʀᴛ : <a href='https://t.me/+4FN95cDmy4IwZDll'>KR_Sᴜᴘᴘᴏʀᴛ</a>
+┃
+┣⪼ Vᴇʀꜱɪᴏɴ : 1.0.0 [ Bᴇᴛᴀ ]
+┃
+┣⪼ Sᴇʀᴠᴇʀ : Hᴇʀᴏᴋᴜ
+┃
+┣⪼ Lᴀɴɢᴜᴀɢᴇ : Pʏᴛʜᴏɴ 3.10.5
+┃
+┣⪼ Fʀᴀᴍᴇᴡᴏʀᴋ : Pʏʀᴏɢʀᴀᴍ 1.4.16
+┃
+┣⪼ Pᴏᴡᴇʀᴇᴅ ʙʏ : @BGM_LinkzZ 
+┃
+┣⪼ Cʀᴇᴅɪᴛs : @----------
+┃
+┣⪼ Lᴀᴜɴᴄʜ Oɴ : [ 16.12.2022 ] 7:30 PM
+┃
+╰─━━─❪ @KR_Botz ❫─━━─⍟ </b>"""
+
     HELP_TXT = """
 <b>🌌 𝐇𝐎𝐖 𝐓𝐎 𝐒𝐄𝐓 𝐓𝐇𝐔𝐌𝐁𝐍𝐈𝐋𝐄 
   
@@ -37,7 +68,7 @@ class kr(object):
 •> /see_caption - Sᴇᴇ Yᴏᴜʀ Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ
 •> /del_caption - Dᴇʟᴇᴛᴇ Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ </b>
 
- 𝗘𝘅𝗮𝗺𝗽𝗹𝗲 :- <code>/set_caption 📕 File Name: {filename}
+ 𝗘𝘅𝗮𝗺𝗽𝗹𝗲 :-<code>/set_caption 📕 File Name: {filename}
 💾 Size: {filesize}
 ⏰ Duration: {duration}
 
@@ -49,25 +80,42 @@ class kr(object):
 
 ®️ Mᴀᴅᴇ Wɪᴛʜ ❣️ @KR_Botz & @BGM_LinkzZ 
 ⚜️ Bᴏᴛ Aɴʏ Issᴜᴇs Cᴏɴᴛᴀᴄᴛ Mᴇ
-@KR_Admin_bot </b> 
+𝗔𝗱𝗺𝗶𝗻 : @KR_Admin_Bot
+𝗦𝘂𝗽𝗽𝗼𝗿𝘁 : @KR_Join </b> 
 """
 
-    DEV_TXT = """
+    DON_TXT = """
 <b>💗 𝐓𝐡𝐚𝐧𝐤𝐬 𝐟𝐨𝐫 𝐬𝐡𝐨𝐰𝐢𝐧𝐠 𝐢𝐧𝐭𝐞𝐫𝐞𝐬𝐭 𝐢𝐧 𝐝𝐨𝐧𝐚𝐭𝐢𝐨𝐧
-
 Dᴏɴᴀᴛᴇ Us Tᴏ Kᴇᴇᴘ Oᴜʀ Sᴇʀᴠɪᴄᴇs Cᴏɴᴛɪɴᴏᴜsʟʏ Aʟɪᴠᴇ 😢
 Yᴏᴜ Cᴀɴ Sᴇɴᴅ Aɴʏ Aᴍᴏᴜɴᴛ 
-Of 20₹, 30₹, 50₹, 70₹, 100₹, 200₹ 😊
 
+Dᴏɴᴀᴛᴇ Oɴʟʏ Oɴᴇ Rᴜᴘᴇᴇ 🥲
+
+Of 10₹, 20₹, 30₹, 50₹, 70₹, 100₹, 200₹ 😊
 📨 Pᴀʏᴍᴇɴᴛ Mᴇᴛʜᴏᴅs:
  
 GᴏᴏɢʟᴇPᴀʏ / Pᴀʏᴛᴏɴ / PʜᴏɴPᴀʏ / PᴀʏPᴀʟ
  
- Oʀ Dᴏɴᴀᴛᴇ: Mᴇssᴀɢᴇ Mᴇ @LastDrogz </b>
-"""
-    START_TXT = """
-<b> I Aᴍ PᴏᴡᴇʀFᴜʟ 🧛‍♂️ Fɪʟᴇs Rᴇɴᴀᴍᴇʀ Bᴏᴛ + Fɪʟᴇ 2 Vɪᴅᴇᴏ Cᴏɴᴇʀᴛᴇʀ BOT Wɪᴛʜ Pᴇʀᴍᴀɴᴇɴᴛ Tʜᴜᴍʙɴᴀɪʟ 💞....!! 
- ʜᴀʀᴇ Aɴᴅ Sᴜᴘᴘᴏʀᴛ Us......!!! </b>
+ Oʀ Dᴏɴᴀᴛᴇ: Mᴇssᴀɢᴇ Mᴇ @MR_Tamil_KiD </b>
 """
 
+    DEV_TXT = "Nothing 🤨"
 
+    PRI_TXT = """
+<b>
+Hᴇʏ! Dᴇᴀʀ Vɪsɪᴛᴏʀs 🚸
+I Cᴀɴ Cʀᴇᴀᴛᴇ A Pᴇʀsᴏɴᴀʟ Bᴏᴛ Fᴏʀ Yᴏᴜ Iɴ Vᴇʀʏ Mɪɴɪᴍᴜᴍ Rᴀᴛᴇ ;
+  
+            Jᴜsᴛ Pᴀʏ 💰 ₹𝟾𝟶/Bᴏᴛ Fᴏʀ Bᴏᴛs. [Sᴏᴍᴇ Bᴏᴛs Lɪᴋᴇ Mᴏᴠɪᴇs Bᴏᴛs Cᴀɴ Cʜᴀʀɢᴇ ₹𝟷𝟼𝟶 Wɪᴛʜ 1𝟶𝟶ᴋ Fɪʟᴇs. ] 
+
+🗣📲📞 Cᴏɴᴛᴀᴄᴛ  Mᴇ : @MR_Tamil_KiD
+
+Pʟᴇᴀsᴇ!!!! Dᴏɴ'ᴛ Dᴇᴍᴀɴᴅs Oᴛᴛ Bᴏᴛs Oʀ Pʀᴇᴍɪᴜᴍ Dᴏᴡɴʟᴏᴀᴅᴇʀ Bᴏᴛs Eʟsᴇ Bᴀɴ!.... 
+
+               ~ Rᴇɢᴀʀᴅs ~
+ ✥════ @KR_Botz ════✥
+
+🤑 Dᴏɴᴀᴛɪᴏɴ Fᴏʀ Us Wʜᴏ Gɪᴠᴇs Cʜᴇᴀᴘᴇsᴛ Pʀɪᴄᴇ Bᴏᴛ :-
+
+ <code> happysfx24@ybl </code> or @MR_Tamil_KiD </b>
+"""
